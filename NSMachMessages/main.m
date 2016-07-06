37c6d9bb97278ad5a17f18f5602fba527c59dfa2
Soaurabh Kakkar <soaurabh.kakkar@monotype.com>
2016-07-07 01:32:44 +0530

    Initial Commit

---
+++ NSMachMessages/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSMachMessages
+//
+//  Created by Soaurabh Kakkar on 07/07/16.
+//  Copyright © 2016 Soaurabh Kakkar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
