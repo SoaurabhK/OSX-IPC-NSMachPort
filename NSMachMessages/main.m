fff5b48ac824f26db855b1751dcbadf9713916eb
Soaurabh Kakkar <soaurabh.kakkar@monotype.com>
2016-07-07 23:42:09 +0530

    Bidirectional communication with reply messages using NSMachPort

--- NSMachMessages/main.m
+++ NSMachMessages/main.m
@@ -10,37 +10,40 @@
 //  but it should work equally well if split into two processes…
 
 #import <Foundation/Foundation.h>
+
 @interface MyServer : NSObject
 @end
 
 @implementation MyServer
+
 + (void)serverThread
 {
     // Register a server's listener port
-    NSMachPort *serverPort = (id)[NSMachPort port];
-    [serverPort setDelegate: (id)self];
-    [serverPort scheduleInRunLoop: [NSRunLoop currentRunLoop] forMode: NSDefaultRunLoopMode];
-    assert([[NSMachBootstrapServer sharedInstance] registerPort: serverPort name: @"SK's Server"]);
-    
-    // Check if server can send messages on client's listener port
-    NSMachPort *serverSendPort = nil;
-    while(!serverSendPort) {
-        serverSendPort = (id)[[NSMachBootstrapServer sharedInstance] portForName: @"SK's Client"];
-    }
-    
-    // Create a message and send it to client's listener port
-    NSPortMessage *message = [[NSPortMessage alloc] initWithSendPort: serverSendPort receivePort:nil
-                                                              components: [NSArray arrayWithObject: [@"hello from server" dataUsingEncoding: NSUTF8StringEncoding]]];
-    [message setMsgid: 0xCBFEBABE];
-    assert([message sendBeforeDate: [NSDate distantFuture]]);
+    NSMachPort *serverRecievePort = (id)[NSMachPort port];
+    [serverRecievePort setDelegate: (id)self];
+    [serverRecievePort scheduleInRunLoop: [NSRunLoop currentRunLoop] forMode: NSDefaultRunLoopMode];
+    assert([[NSMachBootstrapServer sharedInstance] registerPort: serverRecievePort name: @"SK's Server"]);
     
     // To keep the thread alive
     [[NSRunLoop currentRunLoop] run];
     
 }
+
+// Message reciever delegate for server
 + (void)handlePortMessage: (NSPortMessage *)message
 {
     NSLog(@"Server Received message (msgid: 0x%lX): %@, %@, %@", (long)[message msgid], [[NSString alloc] initWithData:message.components[0] encoding:NSUTF8StringEncoding], message.receivePort, message.sendPort);
+    
+    //Check if server can reply message on client's listener port
+    if (!message.sendPort) {
+        return;
+    }
+    
+    // Create a reply message and send it to client's listener port
+    NSPortMessage *replyMsg = [[NSPortMessage alloc] initWithSendPort: message.sendPort receivePort:nil
+                                                          components: [NSArray arrayWithObject: [@"hello from server" dataUsingEncoding: NSUTF8StringEncoding]]];
+    [replyMsg setMsgid: [message msgid]];
+    assert([replyMsg sendBeforeDate: [NSDate distantFuture]]);
     
 }
 @end
@@ -58,7 +61,7 @@
     [clientRecievePort scheduleInRunLoop: [NSRunLoop currentRunLoop] forMode: NSDefaultRunLoopMode];
     assert([[NSMachBootstrapServer sharedInstance] registerPort: clientRecievePort name: @"SK's Client"]);
     
-    
+    // Detach server on the new thread
     [NSThread detachNewThreadSelector: @selector(serverThread) toTarget: [MyServer class] withObject: nil];
     
     // Check if client can send messages on server's listener port
@@ -67,6 +70,7 @@
         clientSendPort = (id)[[NSMachBootstrapServer sharedInstance] portForName: @"SK's Server"];
         
     }
+    
     // Create a message and send it to server's listener port
     NSPortMessage *message = [[NSPortMessage alloc] initWithSendPort: clientSendPort receivePort:clientRecievePort
                                                               components: [NSArray arrayWithObject: [@"hello from client" dataUsingEncoding: NSUTF8StringEncoding]]];
@@ -77,6 +81,7 @@
     [[NSRunLoop currentRunLoop] run];
 }
 
+// Message reciever delegate for client
 + (void)handlePortMessage: (NSPortMessage *)message
 {
     NSLog(@"Client Received message (msgid: 0x%lX): %@, %@, %@", (long)[message msgid], [[NSString alloc] initWithData:message.components[0] encoding:NSUTF8StringEncoding], message.receivePort, message.sendPort);
